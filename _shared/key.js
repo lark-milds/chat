a8eecb41000bac1e85169aa0c412ba8f4accb1ef
r.umehara <r-umehara@concat.jp>
2020-05-13 14:35:19 +0900

    test

---
+++ _shared/key.js
@@ -0,0 +1,1 @@
+window.__SKYWAY_KEY__ = '38a6d2e8-0f48-46ee-8408-4395377f68ef';
---
+++ service_worker.js
@@ -0,0 +1,27 @@
+// キャッシュファイルの指定
+var CACHE_NAME = 'pwa-chat-sample-caches';
+var urlsToCache = [
+    '/192.168.0.243/chat/',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
